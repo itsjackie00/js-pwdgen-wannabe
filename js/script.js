c45f076e90371d1c75bd66262a4a787bd5370899
itsjackie00 <jackiedanso33@gmail.com>
2024-03-05 15:17:47 +0100

    added assigment due

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+/*
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+*/
+
 /* alert('ciao')
 document.write('Hello World !'); */
 
+//userName
+
 let userName = prompt('Come ti chiami ?');
 console.log(userName);
+
